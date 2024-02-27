fc7175b8e11c5756880a7401b1c60afc294c921e
nahidswe <nahidswe@gmail.com>
2024-02-27 11:49:48 +0600

    initial backend added.

---
+++ src/main/java/com/example/springBootWithReactiveRedis/SpringBootWithReactiveRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootWithReactiveRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithReactiveRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithReactiveRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootWithReactiveRedis/SpringBootWithReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootWithReactiveRedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithReactiveRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
